1b55772381f821dd98004438b3d766342c6ff6d4
Marisa Woodland <m1030502@byu.edu>
2026-02-04 16:16:34 -0700

    implemented isInCheckmate - not passing yet

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -181,7 +181,25 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+
+        if(isInCheck(teamColor)) {
+            for(int row = 1; row <= 8; row++) {
+                for(int col = 1; col <= 8; col++){
+                    ChessPosition position = new ChessPosition(row, col);
+                    ChessPiece piece = board.getPiece(position);
+                    if(piece != null) {
+                        TeamColor color = piece.getTeamColor();
+                        if(color == teamColor) {
+                            Collection<ChessMove> moves = validMoves(position);
+                            if(moves.toArray().length != 0) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     /**
