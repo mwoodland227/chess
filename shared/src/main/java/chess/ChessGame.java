3454713c8f8bd65689b9277c833eec1bc6e37f97
Marisa Woodland <m1030502@byu.edu>
2026-02-04 17:31:20 -0700

    finished phase 1

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -256,6 +257,17 @@ public class ChessGame {
         return board;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessGame chessGame = (ChessGame) o;
+        return Objects.equals(board, chessGame.board) && currentTurn == chessGame.currentTurn;
+    }
 
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(board, currentTurn);
+    }
 }
