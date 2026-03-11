2161359f1ee6fea55aff6d6f4edb83c10d584410
Marisa Woodland <m1030502@byu.edu>
2026-03-10 19:56:13 -0600

    implemented executeUpdate

--- server/src/main/java/dataaccess/MySqlGame.java
+++ server/src/main/java/dataaccess/MySqlGame.java
@@ -2,10 +2,11 @@ package dataaccess;
 
 import dataclasses.GameData;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Collection;
 import java.util.List;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
 
 public class MySqlGame implements GameDAO{
     public MySqlGame() throws DataAccessException{
@@ -48,5 +49,30 @@ public class MySqlGame implements GameDAO{
             throw new DataAccessException(ex.getMessage(), ex.getErrorCode());
         }
 
+    }
+
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try(Connection conn = DatabaseManager.getConnection()){
+            try (PreparedStatement ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)){
+                for (int i = 0; i < params.length; i++){
+                    Object param = params[i];
+                    if(param instanceof String p){
+                        ps.setString(i + 1, p);
+                    }else if (param instanceof Integer p) {
+                        ps.setInt(i + 1, p);
+                    }else if(param == null) {
+                        ps.setNull(i + 1, Types.VARCHAR);
+                    }
+                }
+                ps.executeUpdate();
+                ResultSet rs = ps.getGeneratedKeys();
+                if(rs.next()) {
+                    return rs.getInt(1);
+                }
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage(), e.getErrorCode());
+        }
     }
 }
