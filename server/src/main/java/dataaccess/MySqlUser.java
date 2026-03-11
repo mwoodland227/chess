b5a58bd2d7ddcc45d3c7ce1a3bb4f7012309e102
Marisa Woodland <m1030502@byu.edu>
2026-03-10 18:51:06 -0600

    finished executeUpdate, createStatements

--- server/src/main/java/dataaccess/MySqlUser.java
+++ server/src/main/java/dataaccess/MySqlUser.java
@@ -2,12 +2,14 @@ package dataaccess;
 
 import dataclasses.AuthData;
 import dataclasses.UserData;
+import org.mindrot.jbcrypt.BCrypt;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
 
 public class MySqlUser implements UserDAO{
-    public MySqlUser() {
+    public MySqlUser() throws DataAccessException {
         configureDatabase();
     }
     @Override
@@ -17,7 +19,10 @@ public class MySqlUser implements UserDAO{
 
     @Override
     public void createUser(UserData userData) {
-
+        String clearTextPassword = userData.password();
+        String hashedPassword = BCrypt.hashpw(clearTextPassword, BCrypt.gensalt());
+        var statement = "INSERT INTO user (username, password, email) VALUES (?, ?, ?)";
+        executeUpdate(statement, userData.username(), hashedPassword, userData.email());
     }
 
     @Override
@@ -47,22 +52,22 @@ public class MySqlUser implements UserDAO{
 
     private final String[] createStatements = {
             """
-            CREATE TABLE IS NOT EXISTS user(
-            'id' int NOT NULL AUTO_INCREMENT,
-            'username' varchar(256) NOT NULL,
-            'password' varchar(256) NOT NULL,
-            'email' varchar(256) NOT NULL,
-            PRIMARY KEY('id'),
-            UNIQUE KEY 'username' ('username'),
-            UNIQUE KEY 'email' ('email')
+            CREATE TABLE IF NOT EXISTS user(
+            `id` int NOT NULL AUTO_INCREMENT,
+            `username` varchar(256) NOT NULL,
+            `password` varchar(256) NOT NULL,
+            `email` varchar(256) NOT NULL,
+            PRIMARY KEY (`id`),
+            UNIQUE KEY `username` (`username`),
+            UNIQUE KEY `email` (`email`)
             )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
             """
     };
 
-    private void configureDatabase() throws DataAccessException, SQLException {
+    private void configureDatabase() throws DataAccessException {
         DatabaseManager.createDatabase();
         try(Connection conn = DatabaseManager.getConnection()) {
-            for (String statement : createStatments) {
+            for (String statement : createStatements) {
                 try (var preparedStatement = conn.prepareStatement(statement)) {
                     preparedStatement.executeUpdate();
                 }
@@ -71,5 +76,30 @@ public class MySqlUser implements UserDAO{
                 throw new DataAccessException();
         }
 
+    }
+
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try(Connection conn = DatabaseManager.getConnection()){
+            try (PreparedStatement ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)){
+                for (int i = 0; i < params.length; i++){
+                    Object param = params[i];
+                    if(param instanceof String p){
+                        ps.setString(i + 1, p);
+                    }else if (param instanceof Integer p) {
+                        ps.setInt(i + 1, p);
+                    }else if(param == null) {
+                        ps.setNull(i + 1, Types.VARCHAR);
+                    }
+                }
+                ps.executeUpdate();
+                ResultSet rs = ps.getGeneratedKeys();
+                if(rs.next()) {
+                    return rs.getInt(1);
+                }
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
