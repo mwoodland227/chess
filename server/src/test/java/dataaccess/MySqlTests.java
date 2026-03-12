b57ccd7c5d3b97204d699da3542f239ea1cb6c64
Marisa Woodland <m1030502@byu.edu>
2026-03-11 22:31:13 -0600

    wrote some tests, intellij is doing something weird after restarting, a lot of things are red that weren't before the program closed

--- server/src/test/java/dataaccess/MySqlTests.java
+++ server/src/test/java/dataaccess/MySqlTests.java
@@ -1,4 +1,38 @@
 package dataaccess;
 
+import dataclasses.UserData;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class MySqlTests {
+    private static MySqlUser userDao;
+    private static MySqlGame gameDao;
+
+    @BeforeAll
+    static void setup() throws DataAccessException{
+        userDao = new MySqlUser();
+        gameDao = new MySqlGame();
+    }
+
+    @Test
+    void clearUsersPositive() throws DataAccessException {
+        userDao.createUser(new UserData("user", "password", "email@gmail"));
+        userDao.createUser(new UserData("cameron", "pass", "c@m"));
+
+        userDao.clearUsers();
+
+        assertNull(userDao.getUser("user"));
+        assertNull(userDao.getUser("cameron"));
+    }
+
+    void clearAuthPositive() throws DataAccessException {
+        userDao.createUser(new UserData("cameron", "pass", "c@m"));
+
+        userDao.clearUsers();
+
+        assertNull(userDao.getUser("user"));
+        assertNull(userDao.getUser("cameron"));
+    }
 }
