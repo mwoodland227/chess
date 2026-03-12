a19c52b9c2b0f0b03ad26449e69d9babfa518176
Marisa Woodland <m1030502@byu.edu>
2026-03-11 23:20:20 -0600

    finished writing tests, intelliJ being funky, can't run tests

--- server/src/test/java/dataaccess/MySqlTests.java
+++ server/src/test/java/dataaccess/MySqlTests.java
@@ -2,6 +2,8 @@ package dataaccess;
 
 import dataclasses.UserData;
 import dataclasses.AuthData;
+import dataclasses.GameData;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -111,6 +113,11 @@ public class MySqlTests {
     }
 
     @Test
+    void deleteAuthNegative() throws DataAccessException {
+        userDao.deleteAuth("none");
+    }
+
+    @Test
     void getAuthPositive() throws DataAccessException {
         userDao.createUser(new UserData("cam", "pass", "c@g"));
         userDao.createAuth(new AuthData("token", "cam"));
@@ -126,6 +133,8 @@ public class MySqlTests {
         assertNull(result);
     }
 
+
+
     @Test
     void createGamePositive() throws DataAccessException {
         int gameId = gameDao.createGame("Test Game");
@@ -135,5 +144,75 @@ public class MySqlTests {
         GameData game = gameDao.getGame(gameId);
         assertNotNull(game);
         assertEquals("Test Game", game.gameName());
+    }
+
+    @Test
+    void createGameNegative_emptyName() throws DataAccessException {
+        assertThrows(DataAccessException.class, () -> {
+            gameDao.createGame("");
+        });
+    }
+
+
+    @Test
+    void listGamesPositive() throws DataAccessException {
+        gameDao.createGame("Game 1");
+        gameDao.createGame("Game 2");
+
+        var games = gameDao.listGames();
+        assertEquals(2, games.size());
+    }
+
+    @Test
+    void listGamesNegative() throws DataAccessException {
+        var games = gameDao.listGames();
+        assertTrue(games.isEmpty());
+    }
+
+    @Test
+    void getGamePositive() throws DataAccessException {
+        int gameId = gameDao.createGame("Gettable Game");
+        GameData game = gameDao.getGame(gameId);
+
+        assertNotNull(game);
+        assertEquals(gameId, game.gameID());
+        assertEquals("Gettable Game", game.gameName());
+    }
+
+    @Test
+    void getGameNegative() throws DataAccessException {
+        GameData game = gameDao.getGame(999999);
+        assertNull(game);
+    }
+
+    @Test
+    void updateGamePositive() throws DataAccessException {
+        int gameId = gameDao.createGame("Og Name");
+
+        ChessGame gameState = new ChessGame();
+        GameData updatedGame = new GameData(gameId, "WhitePlayer", "BlackPlayer", "New Name", gameState);
+        gameDao.updateGame(updatedGame);
+
+        GameData fromDb = gameDao.getGame(gameId);
+        assertEquals("New Name", fromDb.gameName());
+        assertEquals("WhitePlayer", fromDb.whiteUsername());
+        assertEquals("BlackPlayer", fromDb.blackUsername());
+    }
+
+    @Test
+    void updateGameNegative_invalidGameId() throws DataAccessException {
+        ChessGame gameState = new ChessGame();
+        GameData fakeGame = new GameData(999999, "White", "Black", "Fake", gameState);
+        gameDao.updateGame(fakeGame);
+    }
+
+    @Test
+    void clearGamesPositive() throws DataAccessException {
+        gameDao.createGame("Game1");
+        gameDao.createGame("Game2");
+
+        gameDao.clearGames();
+
+        assertTrue(gameDao.listGames().isEmpty());
     }
 }
