93b04ee2a5ba47f507f241d8dc6e479032d4d9b1
Marisa Woodland <m1030502@byu.edu>
2026-03-24 20:00:22 -0600

    wrote the tests, not all working

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,6 +1,7 @@
 package client;
 
 import dataclasses.AuthData;
+import dataclasses.GameData;
 import dataclasses.UserData;
 import org.junit.jupiter.api.*;
 import server.Server;
@@ -17,15 +18,11 @@ public class ServerFacadeTests {
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
+        port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
         facade = new ServerFacade("http://localhost:" + port);
     }
 
-//    @BeforeEach
-//    public void setUp(){
-//        facade = new ServerFacade("http://localhost:8081");
-//    }
 
     @AfterAll
     static void stopServer() {
@@ -36,10 +33,7 @@ public class ServerFacadeTests {
 //    @BeforeEach
 //    void clearData(){
 //        try{
-//            var auth = facade.register("test", "test", "test");
-//            if(auth != null) {
-//                facade.logout(auth.authToken());
-//            }
+//            facade.register("cleanTest", "cleanPass", "clean@g");
 //        } catch(Exception ignored){}
 //    }
 
@@ -51,19 +45,86 @@ public class ServerFacadeTests {
     }
 
     @Test
-    void registerNegative() throws ClientException{
-        facade.register("cams", "pass", "c@g");
-        assertThrows(ClientException.class, () ->
-                facade.register("cams", "pass1", "c2@g"));
+    void registerNegative() {
+        assertThrows(ClientException.class, () -> {
+            facade.register("camsR", "pass", "c@g");
+            facade.register("camsR", "pass1", "c2@g");
+        });
+
     }
 
 
-//    @Test
-//    void loginPositive() throws ClientException{
-//        facade.register("cams", "pass", "c@g");
-//        AuthData auth = facade.login("cams", "pass");
-//
-//        assertNotNull(au);
-//    }
+    @Test
+    void loginPositive() throws ClientException{
+        facade.register("login", "pass", "c@g");
+        AuthData auth = facade.login("login", "pass");
+
+        assertNotNull(auth.authToken());
+    }
+
+    @Test
+    void loginNegative(){
+        assertThrows(ClientException.class, () ->
+                facade.login("loginBad", "bad"));
+    }
+
+    @Test
+    void logoutPositive() throws ClientException {
+        facade.register("logout1", "pass", "l@g");
+        var auth = facade.login("logout1", "pass");
+        facade.logout(auth.authToken());
+    }
+
+    @Test
+    void logoutNegative(){
+        assertThrows(ClientException.class, () ->
+                facade.logout("fake"));
+    }
+
+    @Test
+    void createGamePositive() throws ClientException {
+        facade.register("create", "pass", "c@g");
+        var auth = facade.login("create", "pass");
+        GameData game = facade.createGame(auth.authToken(), "testGame");
+        assertEquals("testGame", game.gameName());
+        assertTrue(game.gameID() > 0);
+    }
+
+    @Test
+    void createGameNegative() {
+        assertThrows(ClientException.class, () -> facade.createGame("none", "fake"));
+    }
+
+    @Test
+    void listGamePositive() throws ClientException{
+        facade.register("list1", "pass", "list@g");
+        var auth = facade.login("list", "pass");
+        facade.createGame(auth.authToken(), "listGame");
+        var games = facade.listGames(auth.authToken());
+        assertFalse(games.isEmpty());
+    }
+
+    @Test
+    void listGamesNegative(){
+        assertThrows(ClientException.class, () -> facade.listGames("fake"));
+    }
+
+    @Test
+    void joinGamePositive() throws ClientException{
+        facade.register("join", "pass", "joing@g");
+        var auth = facade.login("join", "pass");
+        GameData game = facade.createGame(auth.authToken(), "joinGame");
+        facade.joinGame(auth.authToken(), game.gameID(), "WHITE");
+        var games = facade.listGames(auth.authToken());
+        assertEquals("join", games.get(0).whiteUsername());
+    }
+
+    @Test
+    void joinGameNegative(){
+        facade.register("join1", "pass", "join1@g");
+        var auth = facade.login("join1", "pass");
+        assertThrows(ClientException.class, () -> facade.joinGame(auth.authToken(), 99999, "WHITE"));
+    }
+
 
 }
